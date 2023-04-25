40ceaf2aa20dee7a0c28aeaa470d67b95cc6e18c
= <=>
2023-04-25 14:12:13 -0500

    adds function to prompt user for input

--- src/main/java/AttendanceTracker/SemesterOptions.java
+++ src/main/java/AttendanceTracker/SemesterOptions.java
@@ -4,10 +4,37 @@ import java.util.*;
 
 public class SemesterOptions {
     
-    public void displaySemestersFromMap(HashMap<Integer, String> semesters) { 
+    public static void displaySemestersFromMap(HashMap<Integer, String> semesters) { 
         System.out.println("Semesters available:");
         for(int i = 1; i <= semesters.size(); i++) { 
             System.out.print(i +".) "+ semesters.get(i) );
+        }
+    }
+
+    public static int promptUserforSemester(HashMap<Integer, String> semesters) {       
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please select one of the previous semesters by typing the corresponding number.");
+        int choice = 0;
+        try {
+            while (true) {
+                if (input.hasNextInt()) {
+                    choice = input.nextInt();
+                    if (choice >= 1 || choice <= semesters.size()) { 
+                        return choice; 
+                    } else {
+                        System.out.println("Invalid input. ");
+                        displaySemestersFromMap(semesters);
+                    }
+                } else {
+                    System.out.println("Invalid input. Please enter an integer");
+                    displaySemestersFromMap(semesters);
+                    input.next();
+                }
+            }   
+         }
+        finally {
+            input.close(); 
         }
     }
 
