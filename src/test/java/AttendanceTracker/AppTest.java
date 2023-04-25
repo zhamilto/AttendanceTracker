48f017167b4042828c9075a089982bed4509f39a
eperrio1 <elanaperriott@yahoo.com>
2023-04-25 09:19:47 -0500

    add test for selectingClass function

--- src/test/java/AttendanceTracker/AppTest.java
+++ src/test/java/AttendanceTracker/AppTest.java
@@ -6,9 +6,34 @@ package AttendanceTracker;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.ArrayList;
+
+
+
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    
+    @Test void testselectingClass() {
+        List<String> mockList = new ArrayList<>();
+        mockList.add("MATH 1234");
+        mockList.add("MATH 5678");
+        mockList.add("MATH 3579");
+
+        String inputCourse = "MATH 1234";
+        InputStream in = new ByteArrayInputStream(inputCourse.getBytes());
+        System.setIn(in);
+        assertEquals("MATH 1234", App.selectingClass(mockList));
+    }
+
+
+
+ 
 }
+
