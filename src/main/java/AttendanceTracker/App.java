7fcad7c3af2541cd68e53a38d78a5849cb5ed699
Joshua Green <greenjoshua64@gmail.com>
2023-04-24 19:23:40 -0500

    add helper function calls to main

--- src/main/java/AttendanceTracker/App.java
+++ src/main/java/AttendanceTracker/App.java
@@ -10,5 +10,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+       int userchoice = AppHelper.yearOption();
+        int year = AppHelper.yearFromChoice(userchoice);
+        System.out.println(year);
     }
 }
