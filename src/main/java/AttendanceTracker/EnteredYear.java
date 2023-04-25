99a10b630cccd62a547627cd56a33f7c52dacfbd
tbishop25 <tia.bishop25@gmail.com>
2023-04-25 18:29:23 -0500

    Handle case of non int input for user entering year

--- src/main/java/AttendanceTracker/EnteredYear.java
+++ src/main/java/AttendanceTracker/EnteredYear.java
@@ -1,17 +1,31 @@
 package AttendanceTracker;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EnteredYear {
-    static int enteredYear(){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the year: ");
-        int userEnteredYear = scan.nextInt();
-        while( userEnteredYear < 2010 || userEnteredYear > 2023){
-            System.out.println("Please enter a valid year: ");
-            userEnteredYear = scan.nextInt();
+    static int enteredYear() {
+        Scanner scanner = new Scanner(System.in);
+        int userEnteredYear;
+        try{
+            do {
+                System.out.print("Enter the year: ");
+                while (!scanner.hasNextInt()) {
+                    String input = scanner.next();
+                    System.out.printf("\"%s\" is not a valid year.\n", input);
+                    System.out.print("Enter the year: ");
+                }
+                userEnteredYear = scanner.nextInt();
+            } while (userEnteredYear  < 2010 || userEnteredYear > 2023);
+            System.out.println("The entered year is " + userEnteredYear);
+            return userEnteredYear;
+        } catch (InputMismatchException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println("The entered year is " + userEnteredYear);
-        return userEnteredYear;
+
+
     }
-}
+
+
+    }
+
