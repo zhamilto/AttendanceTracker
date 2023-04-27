745a5a40f6f120f2c3ed42c153b3c3c43065381e
eperrio1 <elanaperriott@yahoo.com>
2023-04-27 00:08:35 -0500

    remove selectingclass test from testing file

--- src/test/java/AttendanceTracker/AppTest.java
+++ src/test/java/AttendanceTracker/AppTest.java
@@ -3,6 +3,7 @@
  */
 package AttendanceTracker;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,8 +11,6 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
 import java.util.ArrayList;
-
-
 
 class AppTest {
     @Test void appHasAGreeting() {
@@ -29,11 +28,33 @@ class AppTest {
         String inputCourse = "MATH 1234";
         InputStream in = new ByteArrayInputStream(inputCourse.getBytes());
         System.setIn(in);
-        assertEquals("MATH 1234", App.selectingClass(mockList));
+        // assertEquals("MATH 1234", App.selectingClass(mockList));
+        assertEquals("MATH 1234", SelectingClass.selectingClass(mockList));
     }
-
-
-
  
 }
+
+
+
+
+// class AppTest {
+//     @Test void appHasAGreeting() {
+//         App classUnderTest = new App();
+//         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+//     }
+
+    
+//     @Test void testselectingClass() {
+//         List<String> mockList = new ArrayList<>();
+//         mockList.add("MATH 1234");
+//         mockList.add("MATH 5678");
+//         mockList.add("MATH 3579");
+
+//         String inputCourse = "MATH 1234";
+//         InputStream in = new ByteArrayInputStream(inputCourse.getBytes());
+//         System.setIn(in);
+//         assertEquals("MATH 1234", App.selectingClass(mockList));
+//     }
+ 
+// }
 
