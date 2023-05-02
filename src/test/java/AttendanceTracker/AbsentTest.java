1fe757c57c2d65246c24f2460619b996c35c085e
tbishop25 <tia.bishop25@gmail.com>
2023-05-01 20:33:37 -0500

    Add test file

---
+++ src/test/java/AttendanceTracker/AbsentTest.java
@@ -0,0 +1,18 @@
+package AttendanceTracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Calendar;
+import java.util.InputMismatchException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AbsentTest {
+    @Test
+    void selectAll() {
+
+    }
+
+}
