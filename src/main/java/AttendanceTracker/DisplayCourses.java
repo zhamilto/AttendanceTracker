d7955a2b5247cf187fcede0c2bdb0151c5432c20
eperrio1 <elanaperriott@yahoo.com>
2023-05-02 01:05:29 -0500

    create displayCoursesList function

--- src/main/java/AttendanceTracker/DisplayCourses.java
+++ src/main/java/AttendanceTracker/DisplayCourses.java
@@ -7,94 +7,78 @@ import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
+
+import com.google.common.collect.MapDifference.ValueDifference;
 
 
 
 public class DisplayCourses {
         // // TODO: Create funciton that calls the year and semester function to create word ie (Spring 2023)
-        // public static String buildSemesterYear() {
-        //     // worked
-        //     YearOptions year = new YearOptions();
-        //     year.userChooseYearStyle();
+        public static String buildSemesterYear() {
+            // worked
+            YearOptions year = new YearOptions();
+            year.userChooseYearStyle();
+
+            // String strYear = String.format("%d", year);
 
 
-        //     // HOW DO YOU RUN JOSH'S CODE 
-        //     // SemesterOptions semester = new SemesterOptions();
-        //     // semester.displaySemestersFromMap(null);
-        //     SemesterOptions.displaySemestersFromMap(null);
-        //     return null;
-        // }
+            // HOW DO YOU RUN JOSH'S CODE 
+            // SemesterOptions semester = new SemesterOptions();
+            // // semester.displaySemestersFromMap(null);
+            // SemesterOptions.displaySemestersFromMap(null);
+            return null;
+        }
 
         
+
+        // On the class view, the user can click "Display List of Courses" button to see 
+        // a list of every class for the given semester and year 
+
 
         // TODO: Display list of couses: 
         // Create function that reads in the txt file, call word function, and returns list of courses 
-        // public static String readFile(String string) throws Exception{
-        //     File file = new File("C:\\Users\\elana\\AttendanceTracker\\src\\main\\java\\AttendanceTracker\\Course.txt");
-        //     Scanner sc = new Scanner(file);
 
-        //     while (sc.hasNextLine())
-        //         System.out.println(sc.nextLine());
-        //     return null;
-        // }
+        static void displayCourseList(String semesterYear){
+            List<String> data = readFile("C:\\Users\\elana\\AttendanceTracker\\src\\main\\java\\AttendanceTracker\\Courses.txt");
+            Map<String, List<String>>courseMap = listIntoMap(data);
 
-        // public static Map<String, List<String>> fileIntoMap(String filePath){
-        //     Map<String, Integer> mapFileContents = new HashMap<String, Integer>();
-        //     BufferedReader br = null;
-
-        //     try{
-        //         File file = new File(filePath);
-
-        //         br = new BufferedReader(new FileReader(file));
-        //         String line = null;
-
-        //         while ((line = br.readLine()) != null){
-        //             String[] part = line.split(","); 
-
-        //             String key = part[0].trim();
-        //             String value = String.parsestring(key[1].trim());
-
-        //             if (!key.equals("") && !value.equals(""))
-        //             mapFileContents.put(key, value);
-        //         }
-        //     }
-        // }
-        
-        
-
-        // static List<String>readFile(String filename){
-        //     ArrayList<String>trend_list=new ArrayList<>();
-        //     try{
-        //         Scanner fileScan=new Scanner(new File(filename));
-        //         while(fileScan.hasNext()){
-        //             String line=fileScan.nextLine();
-        //             trend_list.add(line);
-        //             }  
-        //         }catch(FileNotFoundException e){
-        //             System.out.println("Could not find '"+ filename +"' ");  
-        //         }
-        //     return trend_list;
-        // }
-
-        // static List<String> readFile(String filename){
-        //     List<String> classList = new ArrayList<>();
-        //     try{
-        //         Scanner startScan = new Scanner(new File(filename));
-        //         String headers = startScan.nextLine();
-        //         while (startScan.hasNext()){
-        //             String lineOfData = startScan.nextLine();
-        //             String [] parts = lineOfData.split(",");
-        //             String semesterYear = parts[1];
-        //             String courses = parts[2];
-        //             }
-        //     startScan.close();
-        //     } catch (Exception e){
-        //     System.err.println("An Error has occured");
-        //     System.exit(0);
-        //     }
             
-        //     return classList;            
-        // }
+
+            for(Map.Entry mp: courseMap.entrySet()){
+                if (mp.getKey().equals(semesterYear)){
+
+                    System.out.println(mp.getValue());
+                    
+                    // for (String element : mp.getValue()){
+                    //     System.out.println(element);
+                    // }
+                 
+                } 
+            }
+
+
+            // for(Map.Entry mp: courseMap.entrySet()){
+            //     if (mp.getKey().equals(semesterYear)){
+            //     System.out.println(mp.getValue()); 
+            //     } 
+            //     }
+
+            // for (String key: courseMap.keySet()){
+            //     // System.out.println("----" + semesterYear);
+            //     // System.out.println(key);
+                
+            //     if (semesterYear != key){
+            //         for (List<String> values: courseMap.values()){
+            //             System.out.println(values);
+            //         }
+
+
+           
+
+
+        }
+
             
         static List<String>readFile(String filename){
             ArrayList<String>trend_list=new ArrayList<>();
@@ -109,7 +93,6 @@ public class DisplayCourses {
                 }
             return trend_list;
         }
-
 
         static Map<String, List<String>> listIntoMap(List <String> inputList){
             Map<String, List<String>> map = new HashMap<>();        
@@ -130,7 +113,8 @@ public class DisplayCourses {
 
 
 
-
+        // On the class view, after a "Display List ot Coueses" radio buttion is clicked
+        // the user can select one couses 
         // TODO: Display list of couses -> Sect one course:
         // Attach display list of couses function to the selecting class function 
 
