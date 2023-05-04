774f02479074184b7ac22c9e918ff3f4c9c7b369
tbishop25 <tia.bishop25@gmail.com>
2023-05-04 14:59:35 -0500

    Add functions for choosing email template

--- src/main/java/AttendanceTracker/FormatEmail.java
+++ src/main/java/AttendanceTracker/FormatEmail.java
@@ -1,4 +1,16 @@
 package AttendanceTracker;
 
 public class FormatEmail {
+    //On the roster view, after selecting students incorrectly marked absent, the user can click the preview email button.
+
+    public void chooseEmailFormatTemplate(){
+
+    }
+    public void mistakenlyMarkedAbsentFormat(){
+
+    }
+
+    public void cardSwipeCodeNotEntered(){
+
+    }
 }
