9fdac316f2a65e708a32506222084250d1646e3e
tbishop25 <tia.bishop25@gmail.com>
2023-05-03 22:59:26 -0500

    remove file meant for other branch

--- src/main/java/AttendanceTracker/FormatEmail.java
+++
@@ -1,6 +0,0 @@
-package AttendanceTracker;
-
-public class FormatEmail {
-    //On the roster view, after selecting students incorrectly marked absent, the user can click the preview email button.
-
-}
