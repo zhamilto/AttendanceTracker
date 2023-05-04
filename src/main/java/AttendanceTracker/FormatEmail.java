ae8508569a7db14629ce36ceb516f863976f2754
tbishop25 <tia.bishop25@gmail.com>
2023-05-03 22:51:24 -0500

    Handles case of user not entering S or A

--- src/main/java/AttendanceTracker/FormatEmail.java
+++ src/main/java/AttendanceTracker/FormatEmail.java
@@ -1,2 +1,6 @@
-package AttendanceTracker;public class FormatEmail {
+package AttendanceTracker;
+
+public class FormatEmail {
+    //On the roster view, after selecting students incorrectly marked absent, the user can click the preview email button.
+
 }
