b7a68b3b0a54ec2980ecf1c4d32ac5e3c379f44c
tbishop25 <tia.bishop25@gmail.com>
2023-05-07 22:15:49 -0500

    Begin email format for single student

--- src/main/java/AttendanceTracker/FormatEmail.java
+++ src/main/java/AttendanceTracker/FormatEmail.java
@@ -1,5 +1,6 @@
 package AttendanceTracker;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,6 +15,31 @@ public class FormatEmail {
     static Scanner scan = new Scanner(System.in);
     //On the roster view, after selecting students incorrectly marked absent, the user can click the preview email button.
 
+    static ArrayList<Student>rosterList = readCSVFileIntoList("C:\\Users\\tiabi\\IdeaProjects\\Attendance\\src\\main\\java\\AttendanceTracker\\roster.txt");
+
+    static ArrayList<Student> readCSVFileIntoList(String filename) {
+        ArrayList<Student> studentRosterList = new ArrayList<>();
+        try {
+            Scanner startScan = new Scanner(new File(filename));
+            String headers = startScan.nextLine();
+            while (startScan.hasNext()) {
+                String lineOfData = startScan.nextLine();
+                String[] parts = lineOfData.split(",");
+                String firstName = parts[0];
+                String lastName = parts[1];
+                String idNum = parts[2];
+
+                Student st = new Student(firstName, lastName, idNum);
+                studentRosterList.add(st);
+            }
+            startScan.close();
+        } catch (Exception e) {
+            System.err.println("Couldn't read file. Starting with empty list");
+
+        }
+        return studentRosterList;
+    }
+
     public static void chooseEmailFormatTemplate(){
         System.out.println("Which email format would you like to use?: ");
         String choice = scan.nextLine();
@@ -26,9 +52,17 @@ public class FormatEmail {
     Student ID: 900xxxxxx
     Dates: Example... 1/23, 1/25, 1/27, etc..
      */
-    public static ArrayList mistakenlyMarkedAbsentFormat(){
-        ArrayList<String> mistakeList = selectStudent();
-        return mistakeList;
+    public static String mistakenlyMarkedAbsentFormat(){
+        String term = "";
+        String CRN = "";
+        String idNum = "";
+        String date = "";
+        String completeFormat = "";
+        for (Student student : rosterList) {
+            idNum = student.getIdNum();
+        }
+
+        return "";
     }
 
     /*
