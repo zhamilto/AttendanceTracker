8d38051b09627bfa6062287838498b49b7e9dc31
tbishop25 <tia.bishop25@gmail.com>
2023-05-08 18:32:28 -0500

    Properly select all students or correct those marked absent

--- src/main/java/AttendanceTracker/AbsentStudent.java
+++ src/main/java/AttendanceTracker/AbsentStudent.java
@@ -1,9 +1,8 @@
 package AttendanceTracker;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
 import static AttendanceTracker.FormatEmail.idToStudentMap;
 
 
@@ -12,18 +11,24 @@ public class AbsentStudent {
     static Scanner scan = new Scanner(System.in);
     static ArrayList<String>rosterList = readCSVFileIntoList("C:\\Users\\tiabi\\IdeaProjects\\Attendance\\src\\main\\java\\AttendanceTracker\\roster.txt");
 
+    public static void main(String[] args) {
+        markAllStudents(idToStudentMap);
+        selectStudent(idToStudentMap);
 
-    static public void userChooseStudentSelection() {
+    }
+
+    // allows user to choose to select all students or those marked absent incorrectly
+    static public HashMap userChooseStudentSelection() {
         System.out.print("Do you want to (S) select all students or (A) mark present students incorrectly marked absent?: ");
         String letterChoice = scan.nextLine();
         if (letterChoice.equals("S")) {
-            markAllStudents(idToStudentMap);
+            return markAllStudents(idToStudentMap);
         }
         if (letterChoice.equals("A")) {
-            selectStudent(idToStudentMap);
+            return selectStudent(idToStudentMap);
         }else{
             System.out.println("That input is invalid.");
-            userChooseStudentSelection();
+            return userChooseStudentSelection();
         }
     }
 
@@ -50,43 +55,70 @@ public class AbsentStudent {
     }
 
 
-    static ArrayList<String> markAllStudents(Map<String,String> map) {
-        ArrayList<String> marklist = new ArrayList<>();
+    static HashMap<String,String> markAllStudents(Map<String,String> map) {
+        HashMap<String,String> sMap = new HashMap<>();
         ArrayList<String> list = rosterList;
         for(String id: map.keySet()){
-            //System.out.println(id);
             String student = map.get(id);
-            String mark = id + " PRESENT" + "\n";
-            marklist.add(mark);
+            String mark = id + "\n";
+            sMap.put(id,student);
         }
-        System.out.println(marklist);
-        return marklist;
+        System.out.println("These students have all been marked as PRESENT: "+ sMap.values());
+        return sMap;
     }
 
-    static ArrayList<String> selectStudent(Map<String,String> map) {
+    static HashMap<String,String> selectStudent(Map<String,String> map) {
         ArrayList<String> studentList = rosterList;
         ArrayList<String> markedList = new ArrayList<>();
+        HashMap<String,String> sMap = new HashMap<>();
+        String student;
+        String id = "";
+        String name = "";
+        do {
+            System.out.print("Who was incorrectly marked absent? If you have finished entering students, enter (Q): ");
+            student = scan.nextLine();
+            if(!student.equals("Q")) {
+                boolean studentFound = false;
+                for (String idKey : map.keySet()) {
+                    if (map.get(idKey).equals(student)) {
+                        id = idKey;
+                        name = map.get(id);
+                        sMap.put(id, name);
+                        markedList.add(student);
+                        studentFound = true;
+                        break;
+                    }
+                }
+                if (!studentFound) {
+                    System.out.println("That name is not recognized. Please enter a student on roster.");
+                }
+            }
+        }while (!student.equals("Q"));
+        System.out.println("These students have now been marked as PRESENT " + markedList);
+        return sMap;
+    }
+    static HashMap<String,String> selectAbsentStudents(Map<String,String> map){
+        HashMap<String,String>absentMap = new HashMap<>();
         String student;
         do{
-            System.out.print("Who was incorrectly marked absent? If you have finished entering students, enter (Q): ");
+            //TODO create function for just absent students
+            System.out.print("Which students were absent? If you have finished entering students, enter (Q): ");
             student = scan.nextLine();
             if(!student.equals("Q")){
                 for (String id: map.keySet()) {
                     if(map.containsValue(student)){
-                        String present = map.get(id) + " PRESENT";
-                        markedList.add(present);
-
-                    } else if (!map.containsValue(student)) {
-                        System.out.println("That name is not recognized. Please enter a student on roster.");
-                    } else{
-                        String absent = id + " ABSENT";
-                        markedList.add(absent);
+                        String present = map.get(id);
+                        absentMap.put(id,present);
                     }
-
+                    else if (!map.containsValue(student)) {
+                        System.out.println("That name is not recognized. Please enter a student on roster.");
+                    }
                 }
-                }
+            }
         }while (!student.equals("Q"));
-        System.out.println("These students have now been marked as PRESENT " + markedList);
-        return markedList;
+        System.out.println("These students have now been marked as ABSENT " + absentMap.values());
+        return absentMap;
     }
+    
+    
 }
