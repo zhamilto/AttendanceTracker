0e3d4b06d23d9d6aee2434b74eb6dcad82f37caa
tbishop25 <tia.bishop25@gmail.com>
2023-05-08 21:28:40 -0500

    Add test for students mistakenly marked absent

--- src/test/java/AttendanceTracker/FormatEmailTest.java
+++ src/test/java/AttendanceTracker/FormatEmailTest.java
@@ -1,4 +1,37 @@
 package AttendanceTracker;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.*;
+
+import static AttendanceTracker.AbsentStudent.*;
+import static AttendanceTracker.FormatEmail.idToStudentMap;
+import static AttendanceTracker.FormatEmail.mistakenlyMarkedAbsentFormat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FormatEmailTest {
+    @Test
+    void markAll() {
+        ArrayList<String> list = new ArrayList<>();
+        String term = "Spring 2023";
+        String CRN = "12345";
+        String idList = "900234567";
+        String date = "5/7";
+        String test =  "\n"+ "Term: "+ term +"\n" + "CRN Number: " + CRN + "\n" + "Student ID: "+ idList  + "\n" + "Dates: "+ date + "\n";
+        assertEquals(test,mistakenlyMarkedAbsentFormat());
+    }
+
+//    @Test
+//    void tl() {
+//        ArrayList<String> list = new ArrayList<>();
+//        String term = "Spring 2023";
+//        String CRN = "12345";
+//        String idList = "900234567";
+//        String date = "5/7";
+//        String test =  "\n"+ "Term: "+ term +"\n" + "CRN Number: " + CRN + "\n" + "Student ID: "+ idList  + "\n" + "Dates: "+ date + "\n";
+//        assertEquals(test,mistakenlyMarkedAbsentFormat());
+//    }
+
 }
